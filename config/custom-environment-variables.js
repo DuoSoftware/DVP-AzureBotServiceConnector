40f38e3061ec00950ff01de1512ea518436bea77
sukitha <sukithaj@gmail.com>
2019-01-10 11:00:14 +0530

    First version with azure connector

---
+++ config/custom-environment-variables.js
@@ -0,0 +1,34 @@
+module.exports = {
+
+
+
+
+
+    "Host":
+    {
+		"iss": "COMPANY_NAME",
+        "secret": "HOST_SECRET",
+        "company": "HOST_COMPANY",
+        "tenant": "HOST_TENANT",
+        "token": "HOST_TOKEN"
+
+    },
+
+    "Bot":{
+        "appId": "BOT_APP_ID",
+        "appPassword": "BOT_APP_PASSWORD"
+    },
+
+
+
+    "Services": {
+
+        "mesengerhost": "SYS_EXT_MESSENGER_HOST",//ardsliteservice.app.veery.cloud
+        "messengerport": "SYS_EXT_MESSENGER_PORT",
+        "messengerversion": "SYS_EXT_MESSENGER_VERSION",
+
+        "csaturl": "SYS_CSAT_HOST",
+        "csatport": 'SYS_CSAT_PORT',
+        "csatversion": "SYS_CSAT_VERSION",
+    }
+};
---
+++ config/default.js
@@ -0,0 +1,37 @@
+module.exports = {
+
+
+
+
+
+    "Host":
+    {
+
+        "iss": "singer",
+        "secret": "abcdefgh",
+        "company": 103,
+        "tenant": 1,
+        "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo"
+
+
+    },
+
+    "Bot":{
+        "appId": "0e47e9aa-f9bf-4048-8dcc-2bb82694f7e4",
+        "appPassword": "pdFGQ04$_zzpvlJICT488|+"
+    },
+
+
+    "Services": {
+
+        "messengerhost": "127.0.0.1",//ardsliteservice.app.veery.cloud
+        "messengerport": "8890",
+        "messengerversion": "1.0.0.0",
+        //externalipmessagingservice.app.veery.cloud
+
+        "csaturl": "",
+        //csatservice.app.veery.cloud
+        "csatport": '4445',
+        "csatversion": "1.0.0.0"
+    }
+};
